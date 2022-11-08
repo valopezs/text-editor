3086e57d10b78feffd9cd526d2c5ec85376f956f
valopezs <valeriabrilopez@gmail.com>
2022-11-07 19:21:40 -0600

    added logic to GET content from DB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -35,6 +35,28 @@ export const putDb = async (content) => {
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+    console.log("GET from the database");
+
+    // create a connection to the database and version we want to use
+    const jateDb = await openDB("jate", 1);
+
+    // create a new transaction and specify the database and data privileges
+    const tx = jateDb.transaction("jate", "readonly");
+
+    // open up the desired object store
+    const store = tx.objectStore("jate");
+
+    // use .get() method to get the one text editor entry from the database
+    const request = store.get(1);
+
+    // get confirmation of the request
+    const result = await request;
+    // if there is a test editor entry, return it
+    result
+        ? console.log("Data retrieved from the database! 🗄", result.value)
+        : console.log("Data not found in the database! 🗄");
+    return result?.value;
+};
 
 initdb();

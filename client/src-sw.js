2aac631408902f39bfed3a4c9034e3d1be2af247
valopezs <valeriabrilopez@gmail.com>
2022-11-07 18:51:54 -0600

    implemented asset caching

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,16 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+    ({ request }) => ["style", "script", "worker"].includes(request.destination),
+    new StaleWhileRevalidate({
+      // name of the cache storage
+      cacheName: "asset-cache",
+      plugins: [
+        // this plugin will cache responses with these headers to a maximum-age of 30 days
+        new CacheableResponsePlugin({
+          statuses: [0, 200],
+        }),
+      ],
+    })
+);

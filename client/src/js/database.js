379d719e77bd71fdd31eddd601520634da96cf2b
valopezs <valeriabrilopez@gmail.com>
2022-11-07 18:58:45 -0600

    added connection to db and created transaction

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,18 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+    console.log("PUT to the database");
+
+    // create a connection to the database and version we want to use
+    const jateDb = await openDB("jate", 1);
+
+    // create a new transaction and specify the databse and data privileges
+    const tx = jateDb.transaction("jate", "readwrite");
+
+    // open the desired object store
+    const store = tx.objectStore("jate");
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
